107ed6307deee714c9715a48b96cdc7af2ca1494
H1410101 <H1410101@gmail.com>
2023-05-24 20:42:35 +0800

    Transient: Before Axios

---
+++ trust-submarine-app/src/Search.js
@@ -0,0 +1,18 @@
+import './Search.css';
+
+function Search() {
+    return <div>
+        <LargeLogo/>
+        <Searchbar/>
+    </div>
+}
+
+function LargeLogo() {
+    return <p>Placeholder Large Logo</p>
+}
+
+function Searchbar() {
+    return <p>Placeholder Search Bar</p>
+}
+
+export default Search;
