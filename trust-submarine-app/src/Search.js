301ed019afa3b9745c8635f8214f5fee1a48ce06
H1410101 <H1410101@gmail.com>
2023-05-26 00:26:38 +0800

    Transient Feature: search page

--- trust-submarine-app/src/Search.js
+++ trust-submarine-app/src/Search.js
@@ -1,19 +1,37 @@
 import './Search.css';
 
 function Search() {
-    return <div>
+    return <div class='vertical-flow-flex'>
         <LargeLogo/>
-        <Searchbar/>
+        <SearchForm/>
         <RedirectButton/>
     </div>
 }
 
 function LargeLogo() {
-    return <p>Placeholder Large Logo</p>
+    return <img src='./TrustSubmarine Full Icon.png' class='medium-image'/>
 }
 
-function Searchbar() {
-    return <p>Placeholder Search Bar</p>
+function SearchForm() {
+    return <form>
+        <SearchBar/>
+        <SearchToggle/>
+    </form>
+}
+
+function SearchBar() {
+    return <div id="searchbar">
+        <div id="searchinput-container">
+            <input type="text" name="query" id="searchinput" placeholder="Paste URL here"/>
+        </div>
+        <div id="searchicon-container">
+            <img src="./FlatIcons Magnifying Glass C2E3FF.png" id="searchicon-magglass"/>
+        </div>
+    </div>
+}
+
+function SearchToggle() {
+
 }
 
 function RedirectButton() {
