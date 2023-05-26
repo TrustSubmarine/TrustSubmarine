946edbf57e5e10a4837d8360fecf9e32ba7de394
H1410101 <H1410101@gmail.com>
2023-05-26 20:16:11 +0800

    Feature: searchbar functionality and form submission

--- trust-submarine-app/src/Search.js
+++ trust-submarine-app/src/Search.js
@@ -1,10 +1,10 @@
 import './Search.css';
+import {useState} from 'react';
 
 function Search() {
     return <div class='vertical-flow-flex'>
         <LargeLogo/>
         <SearchForm/>
-        <RedirectButton/>
     </div>
 }
 
@@ -13,36 +13,44 @@ function LargeLogo() {
 }
 
 function SearchForm() {
-    return <form>
+    return <div>
         <SearchBar/>
         <SearchToggle/>
-    </form>
+    </div>
 }
 
 function SearchBar() {
-    return <div id="searchbar">
-        <div id="searchinput-container">
-            <label for="searchinput" class="invisible">Paste URL here</label>
-            <input type="text" name="query" id="searchinput" placeholder="Paste URL here"/>
+
+    var [searchText, setSearchText] = useState("");
+
+    const redirectionHandler = (event) => {
+        event.preventDefault();
+        redirectToSubpage("results?query=" + encodeURIComponent(searchText));
+    }
+
+    return <form onSubmit={redirectionHandler}>
+        <div id="searchbar">
+            <div id="searchinput-container">
+                <label for="searchinput" class="invisible">Paste URL here</label>
+                <input type="text" name="query" id="searchinput" placeholder="Paste URL here" 
+                    onChange={(event) => setSearchText(event.target.value)}/>
+
+            </div>
+            <button id="searchbutton" onClick={redirectionHandler}>
+                <div id="searchicon-container">
+                    <img src="./FlatIcons Magnifying Glass C2E3FF.png" id="searchicon-magglass"/>
+                </div>
+            </button>
         </div>
-        <div id="searchicon-container">
-            <img src="./FlatIcons Magnifying Glass C2E3FF.png" id="searchicon-magglass"/>
-        </div>
-    </div>
+    </form>
+}
+
+function redirectToSubpage(string) {
+    window.location.href = '/' + string;
 }
 
 function SearchToggle() {
 
-}
-
-function RedirectButton() {
-    const handleRedirect = () => {
-        window.location.href = '/subpage';
-    };
-
-    return (
-        <button onClick={handleRedirect}>Redirect to Subpage</button>
-    );
 }
 
 export default Search;
