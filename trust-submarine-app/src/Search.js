a1de8d09f5b448f52b5a2d7c6a27643fe23f37d0
H1410101 <H1410101@gmail.com>
2023-05-27 01:55:28 +0800

    Feature: search type toggle and functionality

--- trust-submarine-app/src/Search.js
+++ trust-submarine-app/src/Search.js
@@ -1,8 +1,13 @@
 import './Search.css';
 import {useState} from 'react';
 
+const SearchType = {
+    KEYWORDS: 'KW',
+    URL: 'URL'
+};
+
 function Search() {
-    return <div class='vertical-flow-flex'>
+    return <div class='verticalflow-medium-flex fullxy'>
         <LargeLogo/>
         <SearchForm/>
     </div>
@@ -13,26 +18,36 @@ function LargeLogo() {
 }
 
 function SearchForm() {
-    return <div>
-        <SearchBar/>
-        <SearchToggle/>
+    var [searchType, setSearchType] = useState(SearchType.URL);
+
+    return <div class='verticalflow-small-flex'>
+        <SearchBar searchType={searchType}/>
+        <SearchToggle searchState={[searchType, setSearchType]}/>
     </div>
 }
 
-function SearchBar() {
 
+/**
+ * React functional component that renders the search bar, including the search button.
+ * Also handles form submission behaviour.
+ * @param props accepts searchType with type SearchType
+ */
+function SearchBar(props) {
     var [searchText, setSearchText] = useState("");
 
     const redirectionHandler = (event) => {
         event.preventDefault();
-        redirectToSubpage("results?query=" + encodeURIComponent(searchText));
+        if (searchText == "") return;
+        redirectToSubpage((props.searchType == SearchType.URL? "product?url=":"results?query=") + encodeURIComponent(searchText));
     }
 
     return <form onSubmit={redirectionHandler}>
         <div id="searchbar">
             <div id="searchinput-container">
                 <label for="searchinput" class="invisible">Paste URL here</label>
-                <input type="text" name="query" id="searchinput" placeholder="Paste URL here" 
+                <input type="text" name="query" id="searchinput" 
+                    value = {searchText}
+                    placeholder={props.searchType==SearchType.KEYWORDS? "Write keywords here":"Paste URL here"}
                     onChange={(event) => setSearchText(event.target.value)}/>
 
             </div>
@@ -45,12 +60,37 @@ function SearchBar() {
     </form>
 }
 
+/**
+ * Redirects to the relative address "domain/string". 
+ * Triggers a full-page refresh.
+ * @param string the subaddress, without the leading "/"
+ */
 function redirectToSubpage(string) {
     window.location.href = '/' + string;
 }
 
-function SearchToggle() {
+/**
+ * React functional component that renders the search type toggle buttons.
+ * Triggers state change on user interaction.
+ * @param props accepts searchState with [searchType, setSearchType]
+ */
+function SearchToggle(props) {
+    var [searchType, setSearchType] = props.searchState;
 
+    return <div id='searchtype-container'>
+        <button id='searchtype-left' 
+            class={'searchtype-button'+(searchType==SearchType.KEYWORDS? ' searchtype-button-active':' searchtype-button-passive')}
+            onClick={(ignored) => setSearchType(SearchType.KEYWORDS)}
+        >
+            Keywords
+        </button>
+        <button id='searchtype-right' 
+            class={'searchtype-button'+(searchType==SearchType.URL? ' searchtype-button-active':' searchtype-button-passive')}
+            onClick={(ignored) => setSearchType(SearchType.URL)}
+        >
+            URL
+        </button>
+    </div>
 }
 
 export default Search;
