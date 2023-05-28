95ae9cc0450d64f0f04540b289834e7c5a7e2aab
H1410101 <H1410101@gmail.com>
2023-05-29 00:54:44 +0800

    Transient Feature: css touchup for product link

--- trust-submarine-app/src/ProductPage/ProductPage.js
+++ trust-submarine-app/src/ProductPage/ProductPage.js
@@ -40,7 +40,7 @@ function ProductPage() {
     return <div class='verticalflow-justify-flex flexfill'>
         <Header/>
         <div id='about-container' class='verticalflow-medium-flex align-center auto-margin'>
-            <p id='product-title'>{productInfo? productInfo.prod_name:""} <a href={productInfo? productInfo.link:""}>amazon</a></p>
+            <p id='product-title'>{productInfo? productInfo.prod_name:""} <a id='link' href={productInfo? productInfo.link:""}>(amazon)</a></p>
             <p id='score'>{productInfo? productInfo.score:""} out of 5</p>
         </div>
     </div>
