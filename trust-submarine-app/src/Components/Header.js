0c9e496d9d51c2f01e3e1fffab395a0863ed2262
H1410101 <33563579+H1410101@users.noreply.github.com>
2023-07-29 16:56:03 +0800

    Transient: Header cleanup

--- trust-submarine-app/src/Components/Header.js
+++ trust-submarine-app/src/Components/Header.js
@@ -5,7 +5,6 @@ import './Header.css'
 function Header() {
     return <div className='horizontalflow-small-flex align-start align-items-end' id='header'>
         <Link to="../about">About</Link>
-        <Link to='../recents'>Recents</Link>
     </div>
 }
 
